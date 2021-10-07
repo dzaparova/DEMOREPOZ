b7e6892ef9d6855beb94ff2fad1f3e323cda542c
dzaparova <dzaparovap@gmail.com>
2021-10-07 15:41:51 +0600

    project init

---
+++ src/Main.kt
@@ -0,0 +1,14 @@
+var myMap:HashMap<String,String>?=null
+fun main (){
+    val names= arrayOf("KG","RU","USA", "TR")
+    val codes= arrayOf("+996","+7","+1","+90")
+    myMap= hashMapOf()
+
+    codes.indices.forEach{
+        myMap?.put(names[it],codes[it])
+    }
+    myMap?.forEach{
+        println("${it.key} ${it.value}")
+    }
+
+}
